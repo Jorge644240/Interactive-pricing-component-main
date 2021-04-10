f224bcb2df20d3d647499b3fb86fbe85eb3219bf
Jorge Gallego <gallegojorge908@gmail.com>
2021-04-10 18:50:40 -0500

    Responsive

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+document.querySelector("#pageviews span").innerText = '100K';
+document.querySelector("#price span").innerText = '$16.00';
+
 document.querySelector("input[type='range']").addEventListener("input", (event) => {
+    if (document.querySelector("input[type=checkbox]").checked === true)
+    {
+        document.querySelector("#pageviews span").innerText = `${event.target.value}K`;
+        document.querySelector("#price span").innerText = `$${(price(event.target.value)*0.75).toFixed(2)}`;   
+    }
+    else
+    {
         document.querySelector("#pageviews span").innerText = `${event.target.value}K`;
         document.querySelector("#price span").innerText = `$${price(event.target.value).toFixed(2)}`;
+    }
+});
+
+document.querySelector("input[type=checkbox]").addEventListener("click", (event) => {
+    if (document.querySelector("input[type=checkbox]").checked === true)
+    {
+        document.querySelector("#pageviews span").innerText = `${document.querySelector("input[type='range']").value}K`;
+        document.querySelector("#price span").innerText = `$${(price(document.querySelector("input[type='range']").value)*0.75).toFixed(2)}`;   
+    }
+    else
+    {
+        document.querySelector("#pageviews span").innerText = `${document.querySelector("input[type='range']").value}K`;
+        document.querySelector("#price span").innerText = `$${price(document.querySelector("input[type='range']").value).toFixed(2)}`;
+    }
 });
 
 function price(views) {
